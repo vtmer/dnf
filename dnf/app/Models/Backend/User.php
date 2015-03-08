a1ce6d5013f5dabfdfbe880b38960ed4abaaa587
Yannyezixin <646840080@qq.com>
2015-03-08 20:16:52 +0800

    管理员用户模型

---
+++ dnf/app/Models/Backend/User.php
@@ -0,0 +1,37 @@
+<?php namespace App\Models\Backend;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+
+class User extends Model implements AuthenticatableContract, CanResetPasswordContract {
+
+    use Authenticatable, CanResetPassword;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'admin_users';
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = ['password', 'remember_token'];
+
+    /**
+     * 根据用户名查询用户
+     *
+     * @return object | null
+     */
+    public static function hasUserByName($username)
+    {
+        return static::where('name', '=', $username)->first();
+    }
+
+}
