2fe7e0d0bdd043689ba353b73f76a6eafed84f4d
Yannyezixin <646840080@qq.com>
2015-03-08 20:15:43 +0800

    后台基础控制器 启用中间件：admin.auth

--- dnf/app/Http/Controllers/Backend/BaseController.php
+++ dnf/app/Http/Controllers/Backend/BaseController.php
@@ -4,4 +4,13 @@ use App\Http\Controllers\Controller;
 
 class BaseController extends Controller {
 
+    /**
+     * 初始化
+     * @middleware: AdminAuth
+     */
+    public function __construct()
+    {
+        $this->middleware('admin.auth');
+    }
+
 }
