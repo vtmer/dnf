584536fd85383db437b97a726cf0c064105f5c94
Yannyezixin <646840080@qq.com>
2015-03-14 02:19:00 +0800

    添加中间件alias

--- dnf/app/Http/Kernel.php
+++ dnf/app/Http/Kernel.php
@@ -28,6 +28,7 @@ class Kernel extends HttpKernel {
 		'auth.basic' => 'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth',
 		'guest' => 'App\Http\Middleware\RedirectIfAuthenticated',
 		'admin.auth' => 'App\Http\Middleware\AdminAuthenticate',
+		'acl.auth' => 'App\Http\Middleware\AclAuthenticate',
 	];
 
 }
