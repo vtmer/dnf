7183e3ba55eb60ea8c723f78cc71efa84e026473
Yannyezixin <646840080@qq.com>
2015-03-14 02:08:00 +0800

    添加时间转换方法

--- dnf/app/Component/Helper.php
+++ dnf/app/Component/Helper.php
@@ -37,4 +37,32 @@ class Helper
         return false;
     }
 
+    /**
+     * 时间处理，转换为
+     * 刚刚, 几秒前，几分钟前，几小时前等
+     *
+     * @param int $time 时间戳
+     * @return string
+     */
+    public static function mdate($time)
+    {
+        $text = '';
+        $time = $time > time() ? time() : $time;
+        $t = time() - $time;
+        $y = date('Y', $time) - date('Y', time());//是否跨年
+
+        if($t == 0) $text = '刚刚';
+        else if($t < 60) $text = $t . '秒前'; // 一分钟内
+        else if($t < 60 * 60) $text = floor($t / 60) . '分钟前'; //一小时内
+        else if($t < 60 * 60 * 24) $text = floor($t / (60 * 60)) . '小时前'; // 一天内
+        else if($t < 60 * 60 * 24 * 3) $text = floor($time/(60*60*24)) == 1 ? '昨天 ' . date('H:i', $time) : '前天 ' . date('H:i', $time) ; //昨天和前天
+        else if($t < 60 * 60 * 24 * 30) $text = date('m/d H:i', $time); //一个月内
+        else if($t < 60 * 60 * 24 * 365 && $y == 0) $text = date('m/d', $time); //一年内
+        else $text = date('Y/m/d', $time); //一年以前
+
+        return $text;
+
+    }
+
+
 }
